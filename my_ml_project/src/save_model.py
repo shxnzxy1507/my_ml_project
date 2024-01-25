884fee6992cb930c8ada655285ef7b5b2543e2c7
shxnzxy1507 <156669510+shxnzxy1507@users.noreply.github.com>
2024-01-25 15:07:28 +0000

    Update save_model.py

--- my_ml_project/src/save_model.py
+++ my_ml_project/src/save_model.py
@@ -6,11 +6,13 @@ from sklearn.linear_model import LinearRegression
 model = LinearRegression()  # Initialize the model if necessary
 model = joblib.load('my_trained_model.joblib')
 
-# Save the model to a file in the src folder
-model_filename = 'src/my_trained_model.joblib'
-joblib.dump(model, model_filename)
+# Save the model to a file in the src folder within the my_ml_project directory
+model_filename = 'my_trained_model.joblib'
+model_filepath = f'my_ml_project/src/{model_filename}'
+joblib.dump(model, model_filepath)
 
-print(f'Model saved locally: {model_filename}')
+print(f'Model saved locally: {model_filepath}')
+
 
 
 
