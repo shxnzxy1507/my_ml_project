78d5d27266a5e9a3525e6bb807e52a9a9a6ca7da
shxnzxy1507 <156669510+shxnzxy1507@users.noreply.github.com>
2024-01-25 18:05:07 +0000

    Update save_model.py

--- my_ml_project/src/save_model.py
+++ my_ml_project/src/save_model.py
@@ -1,6 +1,9 @@
+import os
 import joblib
 from sklearn.linear_model import LinearRegression
 
+#print the current directory
+print("Current working directory is:", os.getcwd())
 # Load the trained model
 model = LinearRegression()
 model = joblib.load('my_trained_model.joblib')
